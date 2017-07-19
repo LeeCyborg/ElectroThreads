95e09cdcb3baea37cd951028c8fb07f205a75b21
Lindy <lindy.wilkins@gmail.com>
2017-07-19 15:28:07 +0100

    Jacket Fade

--- Rebecca/Rebecca.ino
+++ Rebecca/Rebecca.ino
@@ -1,9 +1,11 @@
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
-
-#define PIN 12
+int fadeRate = 0;
+int MaxBrightness = 255;
+int TotalSteps = 10;
+#define PIN 5
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(160, PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
   strip.begin();
@@ -11,22 +13,52 @@ void setup() {
 }
 
 void loop() {
- simpleWave(0.1,5,10);
+  //simpleWave(0.1,5,10); // body piece
+  setSection(0, 100, 0, 20, 255); // jacket begin 
+  delay(1000);
+  setSection(0, 100, 0, 255, 20);
+  delay(1000); // jacket end
+  
 }
-
-void simpleWave(float rate,int cycles, int wait) {
-   float pos=0.0;
-  // cycle through x times
-  for(int x=0; x<(strip.numPixels()*cycles); x++)
-  {
-      pos = pos+rate;
-      for(int i=0; i<strip.numPixels(); i++) {
-        // sine wave, 3 offset waves make a rainbow!
-        float level = sin(i+pos) * 127 + 128;
-        // set color level 
-        strip.setPixelColor(i,0,(int)level,(int)level/15);
-      }
-         strip.show();
-         delay(wait);
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
   }
-}  
+}
+void simpleWave(float rate, int cycles, int wait) {
+  float pos = 0.0;
+  // cycle through x times
+  for (int x = 0; x < (strip.numPixels()*cycles); x++)
+  {
+    pos = pos + rate;
+    for (int i = 0; i < strip.numPixels(); i++) {
+      // sine wave, 3 offset waves make a rainbow!
+      float level = sin(i + pos) * 127 + 128;
+      // set color level
+      strip.setPixelColor(i, 0, (int)level, (int)level / 15);
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+void setSection(int start, int finish, int Nred, int Ngreen, int Nblue) {
+  uint32_t c = strip.getPixelColor(start);
+  uint8_t  redCur = (c >> 16) & 0xFF;
+  uint8_t  greenCur = (c >>  8) & 0xFF;
+  uint8_t  blueCur = c & 0xFF;
+  uint8_t redNew = Nred;
+  uint8_t greenNew = Ngreen;
+  uint8_t blueNew = Nblue;
+  for (int i = 1; i < TotalSteps; i++)
+  {
+    uint8_t red = (((redCur * (TotalSteps - i)) + (redNew * i)) / TotalSteps);
+    uint8_t green = (((greenCur * (TotalSteps - i)) + (greenNew * i)) / TotalSteps);
+    uint8_t blue = (((blueCur * (TotalSteps - i)) + (blueNew * i)) / TotalSteps);
+    for (int j = start; j < finish + 1; j++) {
+      strip.setPixelColor(j, red, green, blue);
+      strip.show();
+      //delay(fadeRate);
+    }
+  }
+}
